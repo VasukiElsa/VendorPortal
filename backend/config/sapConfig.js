0e5948e309716c6db908e70330c21546fc2d404d
VasukiElsa <vasuki.janarthanan6@gmail.com>
2025-07-29 15:00:36 +0530

    Update Depedency files

---
+++ backend/config/sapConfig.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    sapUsername : process.env.SAP_USERNAME,
+    sapPassword : process.env.SAP_PASSWORD,
+    sapBaseUrl : process.env.SAP_BASE_URL
+    
+};
